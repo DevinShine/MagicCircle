94936540fae9482d1eadad3512e1693a841e6919
guopin_wu <505917699@qq.com>
2016-11-20 15:20:18 +0800

    根据代码规范修改变量命名

--- app/src/main/java/com/shadev/pierrebeziercircle/MagicCircle.java
+++ app/src/main/java/com/shadev/pierrebeziercircle/MagicCircle.java
@@ -15,37 +15,29 @@ import android.view.animation.Transformation;
  * 代码写的比较仓猝,以后再优化写法和补充那些数值的具体含义,求勿喷QAQ
  */
 public class MagicCircle extends View {
+	//三阶贝塞尔绘制圆形需要用到的神奇数字
+	private static final float BLACK_MAGIC = 0.551915024494f;
 
 	private Path mPath;
 	private Paint mFillCirclePaint;
 
-	/**
-	 * View的宽度
-	 **/
-	private int width;
-	/**
-	 * View的高度，这里View应该是正方形，所以宽高是一样的
-	 **/
-	private int height;
-	/**
-	 * View的中心坐标x
-	 **/
-	private int centerX;
-	/**
-	 * View的中心坐标y
-	 **/
-	private int centerY;
+	//View的尺寸,View应该是正方形，所以宽高是一样的
+	private int mWidth;
+	private int mHeight;
 
-	private float maxLength;
+	//View的中心坐标
+	private int mCenterX;
+	private int mCenterY;
+
+	private float mMaxLength;
 	private float mInterpolatedTime;
-	private float stretchDistance;
-	private float moveDistance;
-	private float cDistance;
-	private float radius;
+	private float mStretchDistance;
+	private float mMoveDistance;
+	private float mDistance;
+	private float mRadius;
 	private float c;
-	private float blackMagic = 0.551915024494f;
-	private VPoint p2, p4;
-	private HPoint p1, p3;
+	private VPoint mPoint2, mPoint4;
+	private HPoint mPoint1, mPoint3;
 
 
 	public MagicCircle(Context context) {
@@ -68,33 +60,33 @@ public class MagicCircle extends View {
 		mFillCirclePaint.setStrokeWidth(1);
 		mFillCirclePaint.setAntiAlias(true);
 		mPath = new Path();
-		p2 = new VPoint();
-		p4 = new VPoint();
+		mPoint2 = new VPoint();
+		mPoint4 = new VPoint();
 
-		p1 = new HPoint();
-		p3 = new HPoint();
+		mPoint1 = new HPoint();
+		mPoint3 = new HPoint();
 	}
 
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		width = w;
-		height = h;
-		centerX = width / 2;
-		centerY = height / 2;
-		radius = 50;
-		c = radius * blackMagic;
-		stretchDistance = radius;
-		moveDistance = radius * (3 / 5f);
-		cDistance = c * 0.45f;
-		maxLength = width - radius - radius;
+		mWidth = w;
+		mHeight = h;
+		mCenterX = mWidth / 2;
+		mCenterY = mHeight / 2;
+		mRadius = 50;
+		c = mRadius * BLACK_MAGIC;
+		mStretchDistance = mRadius;
+		mMoveDistance = mRadius * (3 / 5f);
+		mDistance = c * 0.45f;
+		mMaxLength = mWidth - mRadius * 2;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		mPath.reset();
-		canvas.translate(radius, radius);
+		canvas.translate(mRadius, mRadius);
 
 		if (mInterpolatedTime >= 0 && mInterpolatedTime <= 0.2) {
 			model1(mInterpolatedTime);
@@ -108,74 +100,74 @@ public class MagicCircle extends View {
 			model5(mInterpolatedTime);
 		}
 
-		float offset = maxLength * (mInterpolatedTime - 0.2f);
+		float offset = mMaxLength * (mInterpolatedTime - 0.2f);
 		offset = offset > 0 ? offset : 0;
-		p1.adjustAllX(offset);
-		p2.adjustAllX(offset);
-		p3.adjustAllX(offset);
-		p4.adjustAllX(offset);
+		mPoint1.adjustAllX(offset);
+		mPoint2.adjustAllX(offset);
+		mPoint3.adjustAllX(offset);
+		mPoint4.adjustAllX(offset);
 
-		mPath.moveTo(p1.x, p1.y);
-		mPath.cubicTo(p1.right.x, p1.right.y, p2.bottom.x, p2.bottom.y, p2.x, p2.y);
-		mPath.cubicTo(p2.top.x, p2.top.y, p3.right.x, p3.right.y, p3.x, p3.y);
-		mPath.cubicTo(p3.left.x, p3.left.y, p4.top.x, p4.top.y, p4.x, p4.y);
-		mPath.cubicTo(p4.bottom.x, p4.bottom.y, p1.left.x, p1.left.y, p1.x, p1.y);
+		mPath.moveTo(mPoint1.x, mPoint1.y);
+		mPath.cubicTo(mPoint1.right.x, mPoint1.right.y, mPoint2.bottom.x, mPoint2.bottom.y, mPoint2.x, mPoint2.y);
+		mPath.cubicTo(mPoint2.top.x, mPoint2.top.y, mPoint3.right.x, mPoint3.right.y, mPoint3.x, mPoint3.y);
+		mPath.cubicTo(mPoint3.left.x, mPoint3.left.y, mPoint4.top.x, mPoint4.top.y, mPoint4.x, mPoint4.y);
+		mPath.cubicTo(mPoint4.bottom.x, mPoint4.bottom.y, mPoint1.left.x, mPoint1.left.y, mPoint1.x, mPoint1.y);
 
 		canvas.drawPath(mPath, mFillCirclePaint);
 
 	}
 
 	private void model0() {
-		p1.setY(radius);
-		p3.setY(-radius);
-		p3.x = p1.x = 0;
-		p3.left.x = p1.left.x = -c;
-		p3.right.x = p1.right.x = c;
+		mPoint1.setY(mRadius);
+		mPoint3.setY(-mRadius);
+		mPoint3.x = mPoint1.x = 0;
+		mPoint3.left.x = mPoint1.left.x = -c;
+		mPoint3.right.x = mPoint1.right.x = c;
 
-		p2.setX(radius);
-		p4.setX(-radius);
-		p2.y = p4.y = 0;
-		p2.top.y = p4.top.y = -c;
-		p2.bottom.y = p4.bottom.y = c;
+		mPoint2.setX(mRadius);
+		mPoint4.setX(-mRadius);
+		mPoint2.y = mPoint4.y = 0;
+		mPoint2.top.y = mPoint4.top.y = -c;
+		mPoint2.bottom.y = mPoint4.bottom.y = c;
 	}
 
 	private void model1(float time) {//0~0.2
 		model0();
 
-		p2.setX(radius + stretchDistance * time * 5);
+		mPoint2.setX(mRadius + mStretchDistance * time * 5);
 	}
 
 	private void model2(float time) {//0.2~0.5
 		model1(0.2f);
 		time = (time - 0.2f) * (10f / 3);
-		p1.adjustAllX(stretchDistance / 2 * time);
-		p3.adjustAllX(stretchDistance / 2 * time);
-		p2.adjustY(cDistance * time);
-		p4.adjustY(cDistance * time);
+		mPoint1.adjustAllX(mStretchDistance / 2 * time);
+		mPoint3.adjustAllX(mStretchDistance / 2 * time);
+		mPoint2.adjustY(mDistance * time);
+		mPoint4.adjustY(mDistance * time);
 	}
 
 	private void model3(float time) {//0.5~0.8
 		model2(0.5f);
 		time = (time - 0.5f) * (10f / 3);
-		p1.adjustAllX(stretchDistance / 2 * time);
-		p3.adjustAllX(stretchDistance / 2 * time);
-		p2.adjustY(-cDistance * time);
-		p4.adjustY(-cDistance * time);
+		mPoint1.adjustAllX(mStretchDistance / 2 * time);
+		mPoint3.adjustAllX(mStretchDistance / 2 * time);
+		mPoint2.adjustY(-mDistance * time);
+		mPoint4.adjustY(-mDistance * time);
 
-		p4.adjustAllX(stretchDistance / 2 * time);
+		mPoint4.adjustAllX(mStretchDistance / 2 * time);
 
 	}
 
 	private void model4(float time) {//0.8~0.9
 		model3(0.8f);
 		time = (time - 0.8f) * 10;
-		p4.adjustAllX(stretchDistance / 2 * time);
+		mPoint4.adjustAllX(mStretchDistance / 2 * time);
 	}
 
 	private void model5(float time) {
 		model4(0.9f);
 		time = time - 0.9f;
-		p4.adjustAllX((float) (Math.sin(Math.PI * time * 10f) * (2 / 10f * radius)));
+		mPoint4.adjustAllX((float) (Math.sin(Math.PI * time * 10f) * (2 / 10f * mRadius)));
 	}
 
 	public void startAnimation() {
